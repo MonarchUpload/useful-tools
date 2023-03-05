428b52d345ea63c709974ed7fc8e9fa88c19956e
MonarchUpload <127032069+MonarchUpload@users.noreply.github.com>
2023-03-05 16:58:53 +0200

    Create get-token.js

---
+++ get-token.js
@@ -0,0 +1,11 @@
+var token = getCookie("token");
+
+if (token) {
+  console.log(token);
+}
+
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
